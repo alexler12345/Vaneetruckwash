f6ff137bee29d346d259c4389ea86e81d588dec0
alexler12345 <156706923+alexler12345@users.noreply.github.com>
2024-11-27 10:52:34 -0700

    v1.02.5.2

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={poppins.className}>
-      
+      <head>
+      <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="VTW" />
+<link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className='m-0 bg-[#f9f9f9]'>
         
         {children}
