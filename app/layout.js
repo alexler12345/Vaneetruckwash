f3f092ce58144926c70bcc7730921e98e2137e4a
alexler12345 <156706923+alexler12345@users.noreply.github.com>
2024-11-28 16:14:58 -0700

    v1.03.1.2.1

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,7 @@
 import {Noto_Sans} from 'next/font/google'
 import "./globals.css";
 import React from 'react';
-import Script from 'next/script';
-import Head from 'next/head';
+
 const poppins = Noto_Sans({
   weight:'700',
   subsets: ['latin'],
@@ -17,28 +16,28 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={poppins.className}>
-      <Head>
+      <head>
 
-      <Script id="google-tag-manager" strategy="afterInteractive">
+      <script id="google-tag-manager" strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-ABCDEFGH');
+            })(window,document,'script','dataLayer','GTM-5MHJN69F');
           `}
-        </Script>
+        </script>
      
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
 <link rel="manifest" href="/site.webmanifest"/>
 
-      </Head>
+      </head>
       <body className='m-0 bg-[#f9f9f9]'>
       <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-ABCDEFGH"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5MHJN69F"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
