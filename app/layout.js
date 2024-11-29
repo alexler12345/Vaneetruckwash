f5bfa07b2dd5a33003e74ddfbd6e4ba88d30ead6
alexler12345 <156706923+alexler12345@users.noreply.github.com>
2024-11-29 10:02:22 -0700

    v1.03.2

--- app/layout.js
+++ app/layout.js
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
   gtag('config', 'G-5LJSLR3S2G');`}
 </script>
 
-     
+   <link rel="icon" href="/favicon.ico"/>
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
