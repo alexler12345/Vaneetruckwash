2a07d84f8dae5b9db8431d6086e63cb6f6763cfa
alexler12345 <156706923+alexler12345@users.noreply.github.com>
2024-12-19 14:49:22 -0700

    no-index gone

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={poppins.className}>
       <head>
-      <meta name="robots" content="noindex"/>
+     
       <script id="google-tag-manager" strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
