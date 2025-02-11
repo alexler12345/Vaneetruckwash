8587f57e801fe673a5ce28b619f54d98621f3bfc
testing-dragon <156706923+alexler12345@users.noreply.github.com>
2025-02-11 12:26:35 -0700

    Update test.js

--- components/subComp/test.js
+++ components/subComp/test.js
@@ -38,13 +38,13 @@ const temp = raw.items
 
 
 //for loop that goes through every item on list
-  for (var i = 0; i <= temp.length; i++) {
+  for (var i = 0; i < temp.length; i++) {
     
     
 //if true then that day is all day
     if (!temp[i].start.dateTime) {
       
-      var k = numtoDay[new Date(temp[i].start.date).getDay() + 1]
+      var k = numtoDay[new Date(temp[i].start.date).getDay() + 2]
       console.log(k)
       officeHours[k].isallday = true
     } //end of if statment
